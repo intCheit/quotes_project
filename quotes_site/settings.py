31a499afa5f1628c1a07f0a453d0b5a28fce9cd7
Serykh Kirill <s.kirill123@mail.ru>
2025-09-02 15:09:37 +0700

    Добавлен STATIC_ROOT для collectstatic

--- quotes_site/settings.py
+++ quotes_site/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -131,3 +132,5 @@ LOGIN_REDIRECT_URL = 'random_quote'
 
 # После выхода пользователя редирект на главную страницу
 LOGOUT_REDIRECT_URL = 'random_quote'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
